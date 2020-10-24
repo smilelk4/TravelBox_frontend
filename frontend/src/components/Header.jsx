ba9f625838bf476c3554d8f4905bc91cf4ee1cba
Yuka <yukamoribe@outlook.com>
2020-10-24 17:18:36 -0400

    Add overlay for header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return ( 
     <header className='header'>
       <div className='header__image-container'>
-        <img src="https://images.pexels.com/photos/2082949/pexels-photo-2082949.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" />
+        {/* <img src="https://images.pexels.com/photos/2082949/pexels-photo-2082949.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" /> */}
       </div>
     </header>
   );
