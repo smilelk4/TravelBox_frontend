4f573a2d4e8840f718c1a761d3c02311112e803b
Yuka <yukamoribe@outlook.com>
2020-10-24 14:50:02 -0400

    Add navbar scroll effect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Section from './components/Section';
 import Footer from './components/Footer';
 
 function App() {
+  // useEffect(() => {
+  //   AOS.init({
+  //     once: true,
+  //     duration: 700,
+  //     easing: 'ease-out-cubic'
+  //   });
+  // });
+
   return (
     <div className="app">
       <Navbar />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 
 const Navbar = () => {
+  const handleScroll = e => {
+    const navbar = document.querySelector('.navbar');
+    if (window.pageYOffset > 10) {
+      navbar.classList.add('scrolled');
+      navbar.classList.remove('initial');
+    } else {      
+      navbar.classList.remove('scrolled');
+      navbar.classList.add('initial');
+    }
+  }
+
+  window.addEventListener('scroll', e => {
+    handleScroll(e);
+  });
+
   return ( 
-    <nav className='navbar'>
+    <nav className='navbar initial'>
       <h1 className='navbar__title'>Travel Box</h1>
       <div className='navbar__hamburger'>X</div>
     </nav>
