c67caed52e3b6cf737acf09195c9a7b0caebf644
Yuka <yukamoribe@outlook.com>
2020-10-25 11:27:44 -0400

    Create User migration and model

---
+++ backend/db/migrations/20201025151035-create-user.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      username: {
+        type: Sequelize.STRING(30),
+        allowNull: false
+      },
+      firstName: {
+        type: Sequelize.STRING(30),
+        allowNull: false
+      },
+      lastName: {
+        type: Sequelize.STRING(30),
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING(55),
+        alloNull: false
+      },
+      hashedPassword: {
+        type: Sequelize.STRING.BINARY,
+        allowNull: false
+      },
+      profileImage: {
+        type: Sequelize.STRING(500),
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ backend/routes/api/users.js
@@ -0,0 +1,13 @@
+// const { Router } = require('express');
+// const { asyncHandler } = require('../../utils');
+
+// const router = Router();
+
+// router.post('/', asyncHandler(async (req, res, next) => {
+ 
+//   res.status(201).json({
+//     user: {
+//       id: 
+//     }
+//   });
+// }));
