0d3bdd09ba77f79de747172d4ab817fd694212d5
Yuka <yukamoribe@outlook.com>
2020-10-28 10:23:33 -0400

    Add scroll animation behavior to MyBox collections

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,16 +1,26 @@
 import React, { useRef, useEffect } from 'react';
 
-const Card = ({ type }) => {
+const Card = ({ type, root }) => {
   const cardDiv = useRef();
 
   useEffect(() => {
     if (type === 'horizontal-1') {
       cardDiv.current.classList.add(type);
     }
-  })
+
+    const observer = new IntersectionObserver(entries => {
+      if (entries[0].intersectionRatio > 0) {
+      cardDiv.current.classList.add('card--visible');
+    } else {
+      cardDiv.current.classList.remove('card--visible');
+    }
+    });
+    observer.observe(cardDiv.current);
+  });
+
 
   return ( 
-    <div ref={cardDiv} className='card'>
+    <div ref={cardDiv} className='card card--animation'>
       <h3>Hiii</h3>
     </div>
   );
--- frontend/src/components/MyBox.jsx
+++ frontend/src/components/MyBox.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import DiagonalContainer from './DiagonalContainer';
 import Card from './Card';
 
 const MyBox = () => {
+  const containerDiv = useRef();
+
   return ( 
     <div className="mybox">
       <DiagonalContainer />
-      <div className="mybox__card-container">
-          <Card type='horizontal-1' />
-          <Card type='horizontal-1' />
-          <Card type='horizontal-1' />
+      <div ref={containerDiv} className="mybox__card-container">
+        <Card type='horizontal-1' root={containerDiv} />
+        <Card type='horizontal-1' root={containerDiv} />
+        <Card type='horizontal-1' root={containerDiv} />
+        <Card type='horizontal-1' root={containerDiv} />
+        <Card type='horizontal-1' root={containerDiv} />
       </div>
     </div>
   );
