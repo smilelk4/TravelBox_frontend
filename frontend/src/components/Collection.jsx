6eff55b2efb3621c58abec393d800a03f7c6d54a
Yuka <yukamoribe@outlook.com>
2020-10-29 00:16:49 -0400

    Solved the issue of not being able to retrieve userInfo before the fetching of the collection detail on MyCollection page

--- frontend/src/components/Collection.jsx
+++ frontend/src/components/Collection.jsx
@@ -1,50 +1,38 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCollection } from '../actions/collectionActions'
 import { DELETE_COLLECTIONS } from '../reducers/collectionReducer'
+import useRestoreUser from '../customHooks/useRestoreUser';
 
 const Collection = props => {
   const dispatch = useDispatch();
   const userInfo = useSelector(state => state.userInfo);
   const collectionId = props.match.params.id;
   const collection = useSelector(state => state.collections[0]);
-
-  // const fetch = useCallback(
-  //   () => dispatch(fetchCollection(collectionId)),
-  //   [dispatch]
-  // );
+  const [isUserLoaded, setisUserLoaded] = useState(false);
 
   useEffect(() => {
-
-    if (!collection) {
-      (async () => {
-        await dispatch(fetchCollection(collectionId));
-      })()
-    }
-
-    return () => dispatch({ type: DELETE_COLLECTIONS });
-
+    (async () => {
+      await dispatch(fetchCollection(collectionId));
+      setisUserLoaded(true);
+    })()
   }, [])
 
-  // console.log(collection.id)
-  // const { collectionName, description, createdAt, updatedAt } = collection;
-  // const { collectionName, description, createdAt, updatedAt } = collection;
-  // useEffect(() => {
-  //   (async () => {
-  //     if(!collection) {
-  //       await dispatch(fetchCollection(collectionId));
-  //     }
-
-  //   console.log(collection, '!!!')
-  // })()
-      // console.log(collection.collectionName)
-      // console.log(collection)
-  // }, []);
-
-  return ( 
-    <div>COllection</div>
-
-  );
+  if (isUserLoaded) {
+    return ( 
+      <>
+        <div>{collection.id}</div>
+        <div>{collection.collectionName}</div>
+        <div>{collection.description}</div>
+        <div>{collection.createdAt}</div>
+        <div>{collection.updatedAt}</div>
+      </>
+    );
+  }
+  return (
+    <div>...Loading</div>
+  )
 }
  
 export default Collection;
