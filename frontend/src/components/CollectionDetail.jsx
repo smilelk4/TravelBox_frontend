97c331f94e551b594b2a312f8284fe2b10c531d2
Yuka <yukamoribe@outlook.com>
2020-10-29 10:58:50 -0400

    Add scrolling behavior and margins for inner cards on CollectionDetail

--- frontend/src/components/CollectionDetail.jsx
+++ frontend/src/components/CollectionDetail.jsx
@@ -11,18 +11,18 @@ const CollectionDetail = ({ id, collectionName, description, createdAt, updatedA
       <DiagonalContainer type="vertical-1" />
       <DiagonalContainer type="vertical-2" />
       <div className="collection-detail">
-      <div className="collection-detail__container">
-        <PageTitle title={collectionName}/>
-      <div className="collection-detail__contents">
-        <img src="http://www.clarksonmotors.co.uk/wp-content/uploads/2015/04/placeholder-600x400.png"></img>
-        <p> { description } </p>
-        {wishes.map(wish => <Card wish={wish} />)}
-      </div>
-      <p className="collection-detail__info">
-        <span>collection id: { id }</span>
-        <span>created at: { createdAt }</span>
-        <span>last updated: { updatedAt }</span>
-      </p>
+        <div className="collection-detail__container">
+          <PageTitle title={collectionName}/>
+          <div className="collection-detail__contents">
+            <img src="http://www.clarksonmotors.co.uk/wp-content/uploads/2015/04/placeholder-600x400.png"></img>
+            <p> { description } </p>
+            {wishes.map(wish => <Card wish={wish} />)}
+          </div>
+          <p className="collection-detail__info">
+            <span>collection id: { id }</span>
+            <span>created at: { createdAt }</span>
+            <span>last updated: { updatedAt }</span>
+          </p>
         </div>
      </div>
     </>
