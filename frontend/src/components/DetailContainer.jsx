f8dc92e15c7f917fe0984bc7a541cc65ca3f4304
Yuka <yukamoribe@outlook.com>
2020-10-29 18:58:46 -0400

    Add add button to detatail container

--- frontend/src/components/DetailContainer.jsx
+++ frontend/src/components/DetailContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PageTitle from './PageTitle';
 import DiagonalContainer from './DiagonalContainer';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 
 const DetailContainer = (props) => {
   return ( 
@@ -8,12 +9,13 @@ const DetailContainer = (props) => {
       <DiagonalContainer type="vertical-1" />
       <DiagonalContainer type="vertical-2" />
       <div className="detail-container">
-        <div className="detail-container__container">
-          <PageTitle title={props.title}/>
-            <div className="detail-container__contents">
-              {props.children}
-            </div>
-        </div>
+      <div className="detail-container__container">
+        <AddCircleIcon className='add-icon' fontSize='large'/>
+        <PageTitle title={props.title}/>
+          <div className="detail-container__contents">
+            {props.children}
+          </div>
+      </div>
       </div>
     </>
   );
