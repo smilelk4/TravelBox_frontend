3b11484e288ed3e27f743183bfd472d54e82baea
Yuka <yukamoribe@outlook.com>
2020-10-31 22:46:25 -0400

    Add images and overlay to header on the main page.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,6 +4,12 @@ const Header = () => {
   return ( 
     <header className='header'>
       <div className='header__image-container'>
+        <div className='header__image'></div>
+        <div className='header__image'></div>
+        <div className='header__image'></div>
+        <div className='header__image'></div>
+        <div className='header__image'></div>
+        <div className='header__image'></div>
         {/* <img src="https://images.pexels.com/photos/2082949/pexels-photo-2082949.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" /> */}
       </div>
     </header>
