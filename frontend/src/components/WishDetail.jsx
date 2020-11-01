97ab4a4226cb1965b24657f602de441e6ad60a86
Yuka <yukamoribe@outlook.com>
2020-11-01 14:40:34 -0500

    Style WishDetails and render image to it.

--- frontend/src/components/WishDetail.jsx
+++ frontend/src/components/WishDetail.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import PageSection from './PageSection';
 import dateDisplay from '../utils/date-converter';
+import DisplayDate from '../utils/date-converter';
 
 const WishDetail = (props) => {
   const { id, description, collectionId, country, regionCity, interestLevel, goalDate, goalSaving, starred, accomplished, createdAt, updatedAt, ToDos: toDos } = props;
+  const images = useSelector(state => state.wishes[0].Images);
+  const [isImageLoaded, setIsImageLoaded] = useState(false); 
+
+  useEffect(() => {
+    if (images) setIsImageLoaded(true);
+  }, [images]);
+  
   return ( 
-    <div>
+    <>
       <div className="wish-detail">
         <div className="wish-detail__container">
+          <div className="detail-container__image-container">
+            {isImageLoaded ? (
+              images.map(img => <img src={img.image} alt={img.image} key={img.image}/> )):( <div>AA</div>)}
+          </div>
           <div className="wish-detail__contents">
             <PageSection label='Description:' value={description}/>
             <PageSection label='Country:' value={country}/>
             <PageSection label='Region/City:' value={regionCity}/>
             <PageSection label='Interest Level:' value={interestLevel}/>
-            <PageSection label='Goal Date:' value={goalDate}/>
+            <PageSection label='Goal Date:' value={ DisplayDate(goalDate) }/>
             <PageSection label='Goal Saving:' value={goalSaving}/>
             <PageSection label='Starred:' value={starred}/>
             <PageSection label='Accomplished:' value={accomplished}/>
@@ -32,7 +45,7 @@ const WishDetail = (props) => {
           </p>
         </div>
      </div>
-    </div>
+    </>
   );
 }
  
